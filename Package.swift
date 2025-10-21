ca89448f786e0bc11a8dc492f0b6143136ac30a3
chenmo <chenmo@qq.com>
2025-10-21 17:37:33 +0800

    1

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RTMModel",
+    
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RTMModel",
+            targets: ["RTMModel"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AgoraIO/AgoraRtm_Apple.git", branch: "2.2.5_lite"),
+        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", .exact("4.6.0")),
+//        .package(url: "https://github.com/AgoraIO/AgoraRtm_Apple.git", .upToNextMajor(from: "2.2.4")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RTMModel",
+            dependencies: [
+//                .product(name: "AgoraRTM", package: "AgoraRtm_Apple"),
+//                    .product(name: "RtcBasic", package: "AgoraRtcEngine_iOS"),
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v5]
+)
